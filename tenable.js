7ac10ffc279082af7f7e400fc5fc37c49285ad0d
สุทธิชัย อินทรัมพรรย์ <intarumpun@yahoo.com>
2020-06-02 13:24:24 +0700

    tenable.js

--- tenable.js
+++ tenable.js
@@ -4,15 +4,11 @@ console.log('Risk-Extension');
 var textProp = 'textContent' in document ? 'textContent' : 'innerText';
 [].slice.call(document.querySelectorAll('span'), 0).forEach(function(aEl) {
   if (aEl[textProp].indexOf('Adobe Flash Player') > -1) {
-    aEl.style.color = '#099700';
-    let begin = aEl[textProp].substring(0,aEl[textProp].indexOf('Adobe Flash Player'));
-    let word = '*********'
-         + aEl[textProp].substring(
-        aEl[textProp].indexOf('Adobe Flash Player') ,
-        aEl[textProp].indexOf('Adobe Flash Player')+18)
-         + '*********';
-    let end = aEl[textProp].substring(aEl[textProp].indexOf('Adobe Flash Player')+18,);
-    aEl[textProp] = begin + word + end;
+    // aEl.style.color = '#099700';
+    var begin = aEl[textProp].substring(0,aEl[textProp].indexOf('Adobe Flash Player'));
+    var word = aEl[textProp].substring(aEl[textProp].indexOf('Adobe Flash Player') ,aEl[textProp].indexOf('Adobe Flash Player')+18);
+    var end = aEl[textProp].substring(aEl[textProp].indexOf('Adobe Flash Player')+18);
+    aEl.innerHTML = begin + "<b style='color:#099700;''>" + word  + "</b>" + end;
   }
 });
 // Test on https://www.tenable.com/plugins/nessus/72383
